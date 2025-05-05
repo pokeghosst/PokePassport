fc550630cdebc79b83403d6279cbc3225cc39796
pokeghost <pokeghost@disroot.org>
2025-05-05 18:57:22 +0300

    feat: Update email without password

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   use_doorkeeper
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -15,6 +19,7 @@ Rails.application.routes.draw do
   end
 
   devise_scope :user do
-    root to: "devise/sessions#new"
+    root to: "users/sessions#new"
+    put "/users/update_email", to: "users/registrations#update_email", as: :update_user_email
   end
 end
